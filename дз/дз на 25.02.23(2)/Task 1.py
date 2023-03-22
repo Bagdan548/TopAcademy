653d86c6256db5d5ab94a8181dd557aa52a19ac9
Bagdan548 <122623436+Bagdan548@users.noreply.github.com>
2023-03-22 17:22:51 +0000

    '''''''''''''''\

---
+++ дз/дз на 25.02.23(2)/Task 1.py
@@ -0,0 +1,6 @@
+c = input()
+if c != c[::-1]: 
+    print("It's not palindrome")
+else:
+    print("It's palindrome")
+input()
---
+++ дз/дз на 25.02.23/Task 3.py
@@ -0,0 +1,5 @@
+numbers = [int(i) for i in input().split(' ')]
+for number in numbers:
+    for divider in range(1, 11):
+            print(f'{number}*{divider}={number * divider} ')
+print('========')

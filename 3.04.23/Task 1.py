bc46b1df1a7106f46ea9c6fec28570ac801c11c4
Bagdan548 <122623436+Bagdan548@users.noreply.github.com>
2023-04-03 16:40:39 +0000

    ---==--=-=-=-=

--- 3.04.23/Task 1.py
+++ 3.04.23/Task 1.py
@@ -1,13 +1,24 @@
-from random import randint 
-A = [randint(-9, 9) for i in range(int(input('n: ')))] 
-print(*A) 
-p, z, n, mini, maxi = 0, 0, 0, 100, -100 
-for a in A: 
-    if a < 0: n += 1 
-    elif a ==0: z += 1 
-    else: p += 1 
-    if a > maxi: maxi = a 
-    if a < mini: mini = a 
-print("min i =", mini, ", maxi =", maxi) 
-print("positives:", p, ", zeroes:", z, ", negatives: ", n) 
-input()
+import random
+n = int(input("Введите размер массива: "))
+mas = []
+s = 1
+sum2 = 0
+sum1 = 0
+sumneg = 0
+sumelem = 0
+mas = [random.randint(-100,100) for i in range(n)]
+print(mas)
+for i in mas:
+      if (i%2) == 0:
+        sum2 += i
+else:
+        sum1 += i
+for i in mas: 
+    if i < 0:
+        sumneg += i
+print("Сумма четных: ",sum2)
+print("Сумма нечетных: ", sum1)
+print("Сумма отрицательных: ", sumneg)
+print("Максимальное значение в массиве: ", max(mas))
+print("Минимальное значение в массиве: ", min(mas))
+print("Сумму элементов, находящихся между первым и последним положительными элементами: ", sum(mas[min(a:= [i for i in range(len(mas)) if mas[i] > 0], default=0):max(a,default=-1) + 1]))

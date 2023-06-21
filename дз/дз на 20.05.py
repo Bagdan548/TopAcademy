36347ba7a4204aa8e15f10e8aa2ca797ca1595fa
Bagdan548 <122623436+Bagdan548@users.noreply.github.com>
2023-06-21 15:02:52 +0000

    /////

--- дз/дз на 20.05.py
+++ дз/дз на 20.05.py
@@ -1,27 +1,30 @@
 class Solution:
+    def clamp(self, val, MIN, MAX):
+        if val < MIN:
+            return MIN
+        if val > MAX:
+            return MAX
+        return val
+
     def myAtoi(self, s: str) -> int:
-        i = 0
-        while i < len(s) and s[i].isspace():
-            i += 1
-        
-        sign = 1
-        if i < len(s) and s[i] in ('-', '+'):
-            if s[i] == '-':
-                sign = -1
-            i += 1
-        
-        num = 0
-        while i < len(s) and s[i].isdigit():
-            num = num * 10 + int(s[i])
-            i += 1
-        
-        num *= sign
-        
-        max_int = (1 << 31) - 1
-        min_int = -(1 << 31)
-        if num > max_int:
-            return max_int
-        elif num < min_int:
-            return min_int
-        else:
-            return num
+        MIN = -1 << 31
+        MAX = -MIN - 1
+        sign = None
+        val = None
+        for c in s:
+            if c == "+" or c == "-":
+                if val != None:
+                    break
+                if sign == None:
+                    sign = [-1, 1][c == "+"]
+                else:
+                    return 0
+            elif 48 <= ord(c) < 58:
+                d = ord(c) - 48
+                val = 10 * (val or 0) + d
+            elif val == None and sign == None:
+                if c != " ":
+                    return 0
+            else:
+                break
+        return self.clamp((sign or 1) * (val or 0), MIN, MAX)
